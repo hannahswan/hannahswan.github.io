353b60ea5522b577f0d88e448efc75607fb71a6d
Hannah <hswan@cs.utah.edu>
2017-12-03 11:20:11 -0700

    Updated files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 // Loads data and initializes views
 
-d3.json('data/codeData.json',function(error,data){
+d3.json('codeData.json',function(error,data){
 
     let funGraph = new FunctionGraph(data);
     let varGraph = new VariableGraph();
